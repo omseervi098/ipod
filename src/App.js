1dc676229e2102d08da72236108c30e28ea30028
Omprakash Choudhary <omseervi098@gmail.com>
2022-09-16 14:39:44 +0530

    added rotatemenu

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import $ from 'jquery';
 class App extends React.Component{
   constructor(){
     super();
+    this.temp_angle=0;
+    // this.temp_selected=0;
     this.state={
       options:['Games','Music','Settings','coverflow'],
       selected:1,
@@ -24,10 +26,21 @@ class App extends React.Component{
     var containerElement=document.getElementsByClassName('buttons-container')[0];
     var zt=new ZingTouch.Region(containerElement);
     zt.bind(containerElement,'rotate',(e)=>{
-      if(e.detail.distanceFromLast>0){
-   
-          console.log('rotate right');
-       }})
+     let dist=e.detail.distanceFromLast;
+      this.temp_angle+=dist;
+      console.log(dist)
+      if(this.temp_angle>90){
+        this.setState({selected:(this.state.selected+1)%this.state.options.length});
+        this.temp_angle=0;   
+      }if(this.temp_angle<-90){
+        let selected=this.state.selected-1;
+        if(selected<0)
+          selected=this.state.options.length-1;
+        this.setState({selected});
+        this.temp_angle=0;
+      }
+
+  })
   }
   menuCliked(){
     
